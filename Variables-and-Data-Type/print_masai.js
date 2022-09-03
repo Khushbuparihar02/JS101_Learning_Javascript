4d0754efbcc3fef6d0753be8080b8d47774e5ef0
Khushbu Parihar <aashiparihar0@gmail.com>
2022-09-03 23:02:56 +0530

    commited

---
+++ Variables-and-Data-Type/print_masai.js
@@ -0,0 +1,4 @@
+// Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+ let Masai_School = "A Transformation in Education";
+console.log(Masai_School);
---
+++ Variables-and-Data-Type/report_card.js
@@ -0,0 +1,30 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+ var name = " khushabu"; 
+console.log("name = khushabu");
+
+var school =" masai";
+console.log("school = masai");
+
+var grade = "A";
+console.log("grade = A");
+
+var section = "A";
+console.log("section = A");
+
+var rollno = 456;
+console.log("rollno = 456");
+
+var hindi = 86;
+console.log("hindi = 86");
+
+var english = 89;
+console.log("english = 89");
+
+var science = 95;
+console.log("science = 95")
+
+var reportcard =  "⁻⁻⁻⁻*****⁻⁻⁻⁻"
+console.log(reportcard);
+       
